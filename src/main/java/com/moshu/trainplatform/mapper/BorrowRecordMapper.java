a27cb8a3b9814e8abe7d74f17a5ed71d69aaf87d
Geek <99729305+qiu121@users.noreply.github.com>
2025-03-25 00:59:29 +0800

    first commit

---
+++ src/main/java/com/moshu/trainplatform/mapper/BorrowRecordMapper.java
@@ -0,0 +1,15 @@
+package com.moshu.trainplatform.mapper;
+
+import com.moshu.trainplatform.entity.BorrowRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author to_Geek
+* @date 2025/03/25
+* @version 1.0
+*/
+@Mapper
+public interface BorrowRecordMapper extends BaseMapper<BorrowRecord> {
+
+}
